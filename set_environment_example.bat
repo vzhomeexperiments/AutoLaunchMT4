6af6b99502cb8c1464435b0e9fa2744d131ebc1a
vzhomeexperiments <vz.home.experiments@gmail.com>
2021-04-25 11:10:54 +0200

    Create set_environment_example.bat

---
+++ set_environment_example.bat
@@ -0,0 +1,11 @@
+::# ----------------------------------------------------------------------------------------
+::# Environmental Variables for Lazytrade System
+::# ----------------------------------------------------------------------------------------
+::# User Environmental Variables
+::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
+
+setx PATH_DSS_Repo "D:\TradingRepos"
+
+setx PATH_T1_T "D:\FxPro - Terminal1"
+setx PATH_T3_T "D:\FxPro - Terminal3"
+
