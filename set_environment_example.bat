551113ef3092b0d02f124d681ced94b303c33ea1
Marty <ziejacode@gmail.com>
2023-05-28 14:42:12 +0100

    Update set_environment_example.bat

--- set_environment_example.bat
+++ set_environment_example.bat
@@ -4,8 +4,10 @@
 ::# User Environmental Variables
 ::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
 
-setx PATH_DSS_Repo "D:\TradingRepos"
+setx PATH_DSS_Repo "C:\Users\Win1\Documents\TradingRepos"
 
-setx PATH_T1_T "D:\FxPro - Terminal1"
-setx PATH_T3_T "D:\FxPro - Terminal3"
+setx PATH_T1_T "C:\Program Files (x86)\TradingTerminals\MetaTrader1 - Terminal1"
+setx PATH_T2_T "C:\Program Files (x86)\TradingTerminals\MetaTrader2 - Terminal2"
+setx PATH_T3_T "C:\Program Files (x86)\TradingTerminals\MetaTrader3 - Terminal3"
+setx PATH_T4_T "C:\Program Files (x86)\TradingTerminals\MetaTrader4 - Terminal4"
 
