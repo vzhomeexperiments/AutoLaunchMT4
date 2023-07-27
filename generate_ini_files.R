16b4dcc1751834ad4de2ac5a06b205b9d1e6f29d
Marty <ziejacode@gmail.com>
2023-07-27 17:26:19 +0100

    upd3

--- generate_ini_files.R
+++ generate_ini_files.R
@@ -17,8 +17,8 @@ path_ini <- file.path(path_dss, 'AutoLaunchMT4')
 # launch MT4 terminal with parameters
 # customize to suit your needs:
 write_ini_file(mt4_Profile = "Default",
-               mt4_Login = "21534387",
-               mt4_Password = "xeuv11",
+               mt4_Login = "21539979",
+               mt4_Password = "rven77",
                mt4_Server = "ICMarketsSC-Demo02",
                dss_inifilepath = path_ini,
                dss_inifilename = "prod_T1.ini",
