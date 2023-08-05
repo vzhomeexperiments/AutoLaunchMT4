e610a561855bbc2ce695f4aa1023df7306c7c41b
Marty <ziejacode@gmail.com>
2023-08-05 09:01:15 +0100

    upd4

--- MetaTraderAutoLaunch.cmd
+++ MetaTraderAutoLaunch.cmd
@@ -5,9 +5,8 @@ rem *************************************************
 ping localhost -n 30
 
 rem Starting terminals with parameters using *.ini files:
-start "1" "%PATH_T1_T%\terminal.exe" /portable "%PATH_DSS_Repo%\AutoLaunchMT4\prod_T1.ini"
-start "2" "%PATH_T2_T%\terminal.exe" /portable "%PATH_DSS_Repo%\AutoLaunchMT4\prod_T2.ini"
-start "3" "%PATH_T3_T%\terminal.exe" /portable "%PATH_DSS_Repo%\AutoLaunchMT4\prod_T3.ini"
-start "4" "%PATH_T4_T%\terminal.exe" /portable "%PATH_DSS_Repo%\AutoLaunchMT4\prod_T4.ini"
+start "1" "%PATH_T1_T%\terminal.exe" /portable "%PATH_DSS_Repo%\CustomExperts\AutoLaunchMT4\prod_T1.ini"
+start "2" "%PATH_T2_T%\terminal.exe" /portable "%PATH_DSS_Repo%\CustomExperts\AutoLaunchMT4\prod_T2.ini"
+
 
 exit
