df6b43bfe9280ce9b796f6e9d792f4b5cf102c22
Marty <ziejacode@gmail.com>
2023-08-08 12:14:32 +0100

    upd6

--- set_environment_example.bat
+++ set_environment_example.bat
@@ -6,8 +6,8 @@
 
 setx PATH_DSS_Repo "C:\Users\Win1\Documents\TradingRepos"
 
-setx PATH_T1_T "C:\Program Files (x86)\TradingTerminals\MetaTrader1 - Terminal1"
-setx PATH_T2_T "C:\Program Files (x86)\TradingTerminals\MetaTrader2 - Terminal2"
-setx PATH_T3_T "C:\Program Files (x86)\TradingTerminals\MetaTrader3 - Terminal3"
-setx PATH_T4_T "C:\Program Files (x86)\TradingTerminals\MetaTrader4 - Terminal4"
+setx PATH_T1_T "C:\Program Files (x86)\TradingTerminals\Terminal1 - Terminal1"
+setx PATH_T2_T "C:\Program Files (x86)\TradingTerminals\Terminal2 - Terminal2"
+setx PATH_T3_T "C:\Program Files (x86)\TradingTerminals\Terminal3 - Terminal3"
+setx PATH_T4_T "C:\Program Files (x86)\TradingTerminals\Terminal4 - Terminal4"
 
