8d60d802a0d8e11d5a3c1f89acf4737956b3d7b4
Marty <ziejacode@gmail.com>
2023-09-10 09:32:56 +0100

    update 6

--- generate_ini_files.R
+++ generate_ini_files.R
@@ -16,23 +16,6 @@ path_ini <- file.path(path_dss, 'AutoLaunchMT4')
 # terminal 1
 # launch MT4 terminal with parameters
 # customize to suit your needs:
-write_ini_file(mt4_Profile = "Default",
-               mt4_Login = "21539979",
-               mt4_Password = "rven77",
-               mt4_Server = "ICMarketsSC-Demo02",
-               dss_inifilepath = path_ini,
-               dss_inifilename = "prod_T1.ini",
-               dss_mode = "prod")
 
-# terminal 2
-# launch MT4 terminal with parameters
-# customize to suit your needs:
-write_ini_file(mt4_Profile = "Default",
-               mt4_Login = "21539975",
-               mt4_Password = "dybj27",
-               mt4_Server = "ICMarketsSC-Demo02",
-               dss_inifilepath = path_ini,
-               dss_inifilename = "prod_T2.ini",
-               dss_mode = "prod")
 
 
--- set_environment_example.bat
+++ set_environment_example.bat
@@ -4,7 +4,7 @@
 ::# User Environmental Variables
 ::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
 
-setx PATH_DSS_Repo "C:\Users\Win1\Documents\TradingRepos"
+setx PATH_DSS_Repo "C:\Users\Win1\Desktop\MQL4PracticeRepo"
 
 setx PATH_T1_T "C:\Program Files (x86)\TradingTerminals\MetaTrader1 - Terminal1"
 setx PATH_T2_T "C:\Program Files (x86)\TradingTerminals\MetaTrader2 - Terminal2"
