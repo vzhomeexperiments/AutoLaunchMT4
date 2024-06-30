4e9e48ec839bd7a48befde3b460a807a12309a0c
Vladimir Zhbanko <vz.home.experiments@gmail.com>
2024-06-30 07:17:33 +0200

    Dump info to user Documents folder

--- Rebootlog.cmd
+++ Rebootlog.cmd
@@ -1,4 +1,17 @@
-echo --------------->> C:\rebootlog.txt
-echo last reboot at:>> C:\rebootlog.txt
-date /T              >> C:\rebootlog.txt
-time /T              >> C:\rebootlog.txt
+:: Reboot logging tool
+:: Purpose: write reboot time to the user Document folder
+@echo off
+setlocal
+:: Define path to the user document folder and respective file
+set "LOG_FILE=%USERPROFILE%\Documents\rebootlog.txt"
+
+:: Log the information
+
+echo ---------------->> %LOG_FILE%
+echo last reboot at: >> %LOG_FILE%
+date /T              >> %LOG_FILE%
+time /T              >> %LOG_FILE%
+
+
+endlocal
+
